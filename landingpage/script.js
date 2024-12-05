319e1e0ab50cd5b7646cda65d7b0a8dffb1fc478
visall-ranganathan <156177349+visallxo@users.noreply.github.com>
2024-12-05 21:42:11 +0530

    Update script.js

--- landingpage/script.js
+++ landingpage/script.js
@@ -2,9 +2,3 @@ function showAlert() {
     alert("Thank you for downloading our White Paper, our team will get in touch with you.");   
     document.getElementById('contactForm').submit();
   }
-
-
-function alert2(){
-    alert('Thank you for registering. Our team will get in touch with you shortly')
-    document.getElementById('clk').submit();
-}
